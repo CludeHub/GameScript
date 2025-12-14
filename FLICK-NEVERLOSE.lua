620307ea96221d335886e1297aa74d1fd5895bd2
CludeHub <149965638+CludeHub@users.noreply.github.com>
2025-12-14 23:09:59 +0800

    Create FLICK-NEVERLOSE.lua

---
+++ FLICK-NEVERLOSE.lua
@@ -0,0 +1,4 @@
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CludeHub/NEVERLOSE/refs/heads/main/NEVERLOSE-FLICK-V2.lua"))()
